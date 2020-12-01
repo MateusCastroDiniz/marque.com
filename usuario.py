f94e34e975059db62919e2be1d8d1ff7e0380de4
Mateus <serv.mateus@gmail.com>
2020-12-01 13:16:01 -0300

    fix: correção de espaços em branco

--- usuario.py
+++ usuario.py
@@ -1,7 +1,7 @@
 from Banco import Banco
 
 
-class Usuarios(object):
+class usuarios(object):
 
     def __init__(self, idusuario="", nome="", telefone="", email="", usuario="", senha=""):
         self.info = {}
